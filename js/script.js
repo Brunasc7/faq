4fb4969425448bc1ed89b856ccc65dba4ba38db1
Brunasc7 <brunasc7@gmail.com>
2023-08-20 21:25:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const iconDown = document.getElementById('icon-down');
+
+// iconDown.addEventListener('click',function teste(){
+//     iconDown.style.transform = 'rotate(180deg)';
+// })
+
+
+// Pegar meu elemento span
